50ce48481edefb3c7ab13bebf71f21582d21a17a
Cristians Soria <cristiansoria@gmail.com>
2023-10-26 22:11:35 -0300

    clean code

--- src/event_emitter.js
+++ src/event_emitter.js
@@ -18,7 +18,6 @@ eventEmitter.on('multiple-start', () => console.log('listener 09'))
 eventEmitter.on('multiple-start', () => console.log('listener 22'))
 
 eventEmitter.on('error', () => console.log("Something was wrong :/"))
-// TODO: eventEmitter.on(errorMonitor, () =>  console.log("Hey! Something was wrong :/"))
 
 console.log('listener count:' + eventEmitter.listenerCount('start'))
 
@@ -32,11 +31,7 @@ eventEmitter.emit('second_deferre')
 eventEmitter.emit('start', { name: 'peter' })
 eventEmitter.emit('start', { name: 'chris' })
 
-// TODO: eventEmitter.emit('error', new Error('whoops!'))
-
 console.log(eventEmitter.listeners('multiple-start').length)
-
-// TODO: console.log(`listenerCount: ${eventEmitter.listenerCount()}`)
 
 eventEmitter.emit('multiple-start')
 
@@ -44,12 +39,9 @@ console.log('event names', eventEmitter.eventNames())
 eventEmitter.removeAllListeners('deferred2')
 console.log('event names post listener removed', eventEmitter.eventNames())
 
-
 eventEmitter.addListener('just-added', () => console.log('just-added'))
 console.log('event names post listener added', eventEmitter.eventNames())
 
-// TODO: eventEmitter.off('just-added', () => console.log('xxx'))
-// console.log('event names post just removed the listener added', eventEmitter.eventNames())
 eventEmitter.prependListener('at-top', () => console.log('at top'))
 console.log('event names post prependListener', eventEmitter.eventNames())
 
